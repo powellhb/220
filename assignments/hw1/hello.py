b20606156a4352b660e7ee3e944a49c97517eccc
Hilary Powell <powellhb@g.cofc.edu>
2021-09-03 17:35:43 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Hilary Powell
+hello.py
+
+Problem: this program tests the environment to see if everything is working.
+
+Certification of Autenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+
+main()
