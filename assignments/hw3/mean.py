ab2808790cf365b78f3e42cc8e1b91c569aded01
Hilary Powell <powellhb@g.cofc.edu>
2021-09-23 22:50:08 -0400

    completed hw3

---
+++ assignments/hw3/mean.py
@@ -0,0 +1,35 @@
+"""
+Name: Hilary Powell
+mean.py
+
+Problem: design a program that outputs the RMS Average, the Harmonic Mean and the Geometric Mean
+
+Certification of Autenticity:
+I certify that this assignment is entirely my own work.
+"""
+#the program will output the rms average, the harmonic mean and the geometric mean
+#the inputs will be a set of numbers and the outputs will be the rms average, the harmonic mean, and the geometric mean
+#first we need to have an area where we can print the amount of values that will need to be entered
+#then create an area that will print a space where you can enter each of those values
+#then have python calculate the three different means of those values entered
+
+import math
+
+def main():
+    print("This program computes the RMS Average, Harmonic Mean, and Geometric Mean respectively")
+    number_of_values = int(input("enter the values to be entered: " ))
+    sum = 0
+    for n in range(number_of_values):
+        print("enter value", n + 1, ":" )
+        value = int(input())
+        sum = sum + value
+    average = sum / number_of_values
+    rms = round(math.sqrt(sum + value**2), 3)
+    print(rms)
+    harmonic_mean = 1 / (number_of_values/sum)
+    print(harmonic_mean)
+    geometric_mean = round((value)**(1/number_of_values), 3)
+    print(geometric_mean)
+
+if __name__ == '__main__':
+    main()
